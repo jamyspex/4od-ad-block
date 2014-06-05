452a524d60e09c1e7b6b4015b2f410accaf695c0
Sam Greenhalgh <github.com@zapthedingbat.com>
2014-06-05 20:42:21 +0100

    Only redirect POST request to stub XML response

--- source/background.js
+++ source/background.js
@@ -1,5 +1,13 @@
 chrome.webRequest.onHeadersReceived.addListener(
-  function() { return {redirectUrl: "data:text/xml,%3CadResponse%20%2F%3E"}; },
+  function(info) {
+	var response = {};
+	console.log("onHeadersReceived", this, arguments);
+	if(info.method == "POST")
+	{
+		response.redirectUrl = "data:text/xml,%3CadResponse%20%2F%3E";
+	}
+	return response;
+  },
   { urls: ["http://mrm.channel4.com/ad/p/*"], types: ["object"] },
   ["blocking"]
 );
