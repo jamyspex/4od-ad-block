67819d5c7a6f93cf30586a4fe900c2d2174bb347
Sam Greenhalgh <github.com@zapthedingbat.com>
2014-06-13 23:36:32 +0100

    Redirect when response content type is text/xml

--- source/background.js
+++ source/background.js
@@ -1,13 +1,20 @@
 chrome.webRequest.onHeadersReceived.addListener(
   function(info) {
 	var response = {};
-	console.log("onHeadersReceived", this, arguments);
-	if(info.method == "POST")
+	if(isTextXml(info.responseHeaders))
 	{
 		response.redirectUrl = "data:text/xml,%3CadResponse%20%2F%3E";
 	}
 	return response;
   },
   { urls: ["http://mrm.channel4.com/ad/p/*"], types: ["object"] },
-  ["blocking"]
+  ["blocking", "responseHeaders"]
 );
+
+var isTextXml = function(headers){
+    for(var i = 0, j= headers.length; i < j; i++){
+        if(headers[i].name === "Content-Type" && headers[i].value === "text/xml"){
+            return true;
+        }
+    }
+}
