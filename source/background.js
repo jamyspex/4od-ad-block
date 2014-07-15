2a77624cafabe48b80834371fc8df1aefe731eb2
Sam Greenhalgh <github.com@zapthedingbat.com>
2014-07-15 08:20:28 +0100

    Version 3.0
    Cancel requests for AdManager.swf

--- source/background.js
+++ source/background.js
@@ -1,75 +1,51 @@
-var playerFileName = "4odplayer-11.79.swf";
+// Expected 4oD Player version
+var playerFileName = "4odplayer-11.80.swf";
 
-function onBeforeRequest (info) {
-
-    // Get the file name of the 4oD player
-    var requestedPlayerFileName = /[^\/]+$/.exec(info.url)[0];
-    
-    // Show default status
-    updateStatus(info.tabId, "Default");
-    
-    // Check for updated player
-    if(requestedPlayerFileName !== playerFileName){
-        newPlayerDetected(info.tabId, requestedPlayerFileName);
-    }
-};
-
-function newPlayerDetected(tabId, requestedPlayerFileName){
-
-    updateStatus(tabId, "Fail");
-    
-    // Check for extension updates
-    chrome.runtime.requestUpdateCheck(function(status){
-        if(status === "update_available"){
-            chrome.runtime.reload();
-        }
-    });
-}
-
-function onHeadersReceived(info) {
-    var response = {};
-    
-    // Check if the response has text/xml content type
-    if(isResponseTextXml(info.responseHeaders))
-    {
-        // Redirect to data URL with empty adResponse node
-        response.redirectUrl = "data:text/xml,%3CadResponse%20%2F%3E";
-        
-        // Feedback to the user that ad blocking was invoked
-        updateStatus(info.tabId, "Success");
-    }
-    
-    return response;
-}
-
-function isResponseTextXml (headers){
-    for(var i = 0, j= headers.length; i < j; i++){
-        if(headers[i].name === "Content-Type" && headers[i].value === "text/xml"){
-            return true;
-        }
-    }
-};
-
-function updateStatus (tabId, status) {
-    var popup = (status === "Fail") ? "popup.html" : "";
-    chrome.pageAction.setPopup({
-        tabId: tabId,
-        popup: popup
-    });
-    chrome.pageAction.setIcon({
-        tabId: tabId,
-        path: "images/pageAction" + status + ".png"});
+// Show appropriate page action in the omnibar
+function updateStatus (tabId, isSuccess) {
+    var popup = isSuccess ? "popupSuccess.html" : "popupFail.html";
+    var iconPath = isSuccess ? "images/pageActionSuccess.png" : "images/pageActionFail.png";
+    chrome.pageAction.setPopup({ tabId: tabId, popup: popup });
+    chrome.pageAction.setIcon({ tabId: tabId, path: iconPath });
     chrome.pageAction.show(tabId);
-};
+}
+
+function onPlayerBeforeRequest(info) {
+	
+	// Get the file name of the 4oD player
+    var requestedPlayerFileName = /[^\/]+$/.exec(info.url)[0];
+
+    // Check for updated player
+    if(requestedPlayerFileName == playerFileName){
+	
+		// Update status to show success indicator
+		updateStatus(info.tabId, true);
+		
+	}else{
+	
+		// Update status to show failure indicator
+		updateStatus(info.tabId, false);
+		
+		// Check for extension updates
+		chrome.runtime.requestUpdateCheck(function(status){
+			if(status === "update_available"){
+				chrome.runtime.reload();
+			}
+		});
+	}
+}
+
+function onAdManagerHeadersReceived(info) {
+    return { cancel: true };
+}
 
 chrome.webRequest.onBeforeRequest.addListener(
-    onBeforeRequest,
-    { urls: ["http://www.channel4.com/static/programmes-flash/swf/4odplayer*"] },
-    []
+    onPlayerBeforeRequest,
+    { urls: ["http://www.channel4.com/static/programmes-flash/swf/4odplayer*"] }
 );
 
-chrome.webRequest.onHeadersReceived.addListener(
-    onHeadersReceived,
-    { urls: ["http://mrm.channel4.com/ad/p/*"], types: ["object"] },
-    ["blocking", "responseHeaders"]
+chrome.webRequest.onBeforeRequest.addListener(
+    onAdManagerHeadersReceived,
+    { urls: ["http://mrmam.channel4.com/p/c4_live/AdManager.swf*"] },
+    ["blocking"]
 );
